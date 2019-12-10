1e3f0d09f8aef95db182c66eac379acd16ca524e
gzougianos <g.zougianos@gmail.com>
2019-12-10 15:50:27 +0200

    save button not enabled when there are no changes to document

--- LatexEditorProject/src/gr/uoi/cs/controller/EditorViewController.java
+++ LatexEditorProject/src/gr/uoi/cs/controller/EditorViewController.java
@@ -1,10 +1,13 @@
 package gr.uoi.cs.controller;
 
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+
 import gr.uoi.cs.controller.commands.Command;
 import gr.uoi.cs.controller.commands.CommandFactory;
 import gr.uoi.cs.view.EditorView;
 
-public class EditorViewController {
+public class EditorViewController implements DocumentListener {
 	private EditorView editorView;
 	private CommandFactory commandFactory;
 
@@ -12,6 +15,7 @@ public class EditorViewController {
 		this.editorView = editorView;
 		this.commandFactory = commandFactory;
 
+		editorView.getEditorComponent().getDocument().addDocumentListener(this);
 		registerCommands();
 	}
 
@@ -26,5 +30,29 @@ public class EditorViewController {
 
 	public void saveDocument() {
 		commandFactory.createCommand(Command.SAVE_DOCUMENT).execute();
+		updateSaveButtonAvailability();
+	}
+
+	@Override
+	public void insertUpdate(DocumentEvent e) {
+		updateSaveButtonAvailability();
+	}
+
+	@Override
+	public void removeUpdate(DocumentEvent e) {
+		updateSaveButtonAvailability();
+	}
+
+	@Override
+	public void changedUpdate(DocumentEvent e) {
+		updateSaveButtonAvailability();
+	}
+
+	private void updateSaveButtonAvailability() {
+		String documentContent = editorView.getCurrentDocument().getContents();
+		String contentInTextComponent = editorView.getEditorComponent().getText();
+		boolean hasNeverBeenSavedBefore = editorView.getCurrentDocument().getPath() == null;
+		editorView.getSaveFileButton()
+				.setEnabled(hasNeverBeenSavedBefore || !documentContent.equals(contentInTextComponent));
 	}
 }
